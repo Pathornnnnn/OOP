3d5cfc68977eeb7fe53d232b9cc340e7a580aa7b
Pathorn <pathornsomsae@gmail.com>
2025-02-22 20:55:43 +0700

    add seq_add_cart

--- path/to/bananun.py
+++ path/to/bananun.py
@@ -1,3 +1,56 @@
+from fasthtml.common import *
+
+app , rt = fast_app()
+
+@rt('/p/Monitor')
+def get():
+    return Titled(
+        "Banana : Select Item Page",
+        Div(
+            Container(
+                P("DELL P2425H 23.8 INCH IPS FHD 100HZ 5MS *จอคอมพิวเตอร์"),
+                Grid(
+                    Img(src="https://storage.googleapis.com/file-computeandmore/large_images/3627867c-c3b1-4db0-b9eb-27b9a4b15ffe.png",style="width: 300px; height: 300px;", alt="ตัวอย่างภาพ"),
+                    style="text-align: center; background-color:rgb(182, 255, 253);"
+                ),
+                Container(
+                    Grid(
+                        
+                        Div(P("ทำงานได้อย่างมีประสิทธิภาพ รับความสบายตาที่เพิ่มมากขึ้นและการเชื่อมต่อที่ราบรื่นด้วยจอภาพ FHD ที่ได้รับการรับรองจาก TÜV ว่าสบายตาในระดับ 4 ดาวจุดเด่นสินค้าลดการปล่อยแสงสีฟ้าอันเป็นอันตรายเหลือ ≤35% เพื่อความสบายตลอดทั้งวันโดยไม่ต้องเสียสละสีสันอัตราการรีเฟรช 100Hz ช่วยลดการสั่นไหว เลื่อนภาพได้ลื่นไหลยิ่งขึ้น และเคลื่อนไหวได้ราบรื่นยิ่งขึ้นครอบคลุมสีที่กว้างโดยมีสีที่แสดงได้สูงสุดถึง 16.7 ล้านสีที่ 99% sRGBสีสันสดใสในมุมมองที่กว้างด้วยเทคโนโลยี In-Plane Switching (IPS)"), cls="box")
+                    ),
+                    Button("Add Cart",style="margin-right: 20px; text-align: center;"),Button("Purchase",style="text-align: center;"),
+                    style="text-align: center; background-color:rgb(94, 93, 93);"
+                )     
+            )
+        )
+
+    )
+
+@rt('/p/Keyboard')
+def get():
+    return Titled(
+        "Banana : Select Item Page",
+        Div(
+            Container(
+                P("คีย์บอร์ดเกมมิ่ง Signo Gaming Keyboard Nuzzon KB-751 Wireless Mechanical Black (Blue Switch)"),
+                Grid(
+                    Img(src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp7b8xCcf89FHFziDHJ8Asl1k3dHk49AlMgA&s",style="width: 300px; height: 300px;", alt="ตัวอย่างภาพ"),
+                    style="text-align: center; background-color:rgb(182, 255, 253);"
+                ),
+                Container(
+                    Grid(
+                        
+                        Div(P("KB 751 NUZZON PUDDING WIRELESS OPTICAL SWITCH KEYBOARD การส่งคำสั่งและการตอบสนองที่เร็วกว่าเพียง 2 MS ทำงานด้วยระบบ INFARED ตอบสนองได้เร็วกว่า มีความทนทานและแก้ปัญหาปุ่มเบิ้ล รองรับการกดได้มากถึง 100 ล้านครั้ง  ใช้งานได้นานสูงสุด 20 ชม. ( แบบปิดไฟ ) 10 ชม. (แบบเปิดไฟ) วัสดุเป็นพลาสติก ABS แข็งแรง ทนทาน  มีฟังค์ชั่นล็อควินโดว์ ปุ่มคีย์บอร์ดแบบจมเพิ่มความเรียบหรู พร้อมอะแดปเตอร์ขยายตัวรับสัญญาน"), cls="box")
+                    ),
+                    Button("Add Cart",style="margin-right: 20px; text-align: center;"),Button("Purchase",style="text-align: center;"),
+                    style="text-align: center; background-color:rgb(94, 93, 93);"
+                )     
+            )
+        )
+
+    )
+
+
 class System:
     def __init__(self, acc_lst, product_lst):
         self.__acc_lst = acc_lst
@@ -106,3 +159,5 @@ print('ID :',jj.get_acc_id(),'| Name :',  jj.get_name() ,'| Cart :', jj.get_cart
 
 
 
+
+serve()
---
+++ path/to/seq_add_cart.py
@@ -0,0 +1,117 @@
+class System:
+    def __init__(self, acc_lst, product_lst):
+        self.__acc_lst = acc_lst
+        self.__product_lst = product_lst
+
+    def search_acc_by_id(self,id):
+        for acc in self.__acc_lst:
+            if acc.get_acc_id() == id:
+                return acc
+        
+    def search_product_by_name(self,name):
+        for product in self.__product_lst:
+            if name == product.get_name_product():
+                return product
+    def get_acc_lst(self):
+        return self.__acc_lst
+    
+    def get_product_lst(self):
+        return self.__product_lst
+
+    def Add_to_cart(self, product_name , quantity, acc_id):
+        product = self.search_product_by_name(product_name)
+        acc = self.search_acc_by_id(acc_id)
+        stock_product = product.get_stock_product()
+        if stock_product >= quantity:
+            itemm = Cartitem(product,quantity)
+            acc.Add_to_cart_shopping(itemm)
+            return True
+
+        else:
+            return False
+
+class Account:
+    def __init__(self,user_id , name , email):
+        self.__id = user_id
+        self.__name = name
+        self.__email = email
+        self.__myCart_shopping = Cart([])
+    
+    def get_acc_id(self):
+        return self.__id
+
+    def get_name(self):
+        return self.__name
+    
+    def Add_to_cart_shopping(self,cartitemm):
+        self.__myCart_shopping.Add_to_cartitem(cartitemm)
+
+    def get_cart_shopping(self): 
+        return self.__myCart_shopping
+    
+class Product:
+    def __init__(self,product_id , name , stock):
+        self.__id = product_id
+        self.__name = name
+        self.__stock = stock
+    
+    def get_name_product(self):
+        return self.__name
+
+    def get_stock_product(self):
+        return self.__stock
+
+class Cart:
+    def __init__(self,Cartitem_lst=[]):
+        self.__Cartitem_lst = Cartitem_lst
+
+    def Add_to_cartitem(self,inp):
+        self.__Cartitem_lst.append(inp)
+
+    def __str__(self):
+        return f'{[[i.get_product().get_name_product(), i.get__quantity() ] for i in self.__Cartitem_lst]}'
+
+class Cartitem:
+    def __init__(self, product , quantity):
+        self.__product = product
+        self.__quantity = quantity
+
+    def get_product(self):
+        return self.__product
+    
+    def get__quantity(self):
+        return self.__quantity
+    
+    def __str__(self):
+        return f'{self.__product} : {self.__quantity}'
+
+#init usr
+user_lst = []
+maxkey = Account("1",'MAX','MAX@gmail.com')
+jj = Account("2",'JJ','JJ@gmail.com')
+user_lst.append(maxkey)
+user_lst.append(jj)
+
+#init product
+product_lst = []
+a = Product(1,'Monitor',10)
+b = Product(2,'Keyboard',10)
+product_lst.append(a)
+product_lst.append(b)
+bananaIT = System(user_lst,product_lst)
+
+
+#test add cart
+print('ID :',maxkey.get_acc_id(),'| Name :',  maxkey.get_name() ,'| Cart :', maxkey.get_cart_shopping())
+print('ID :',jj.get_acc_id(),'| Name :',  jj.get_name() ,'| Cart :', jj.get_cart_shopping())
+
+# test case
+# เพิ่มสินค้า Monitor จำนวน 2 ชิ้น ลงตะกร้าของไอดี 1 (MAX)
+bananaIT.Add_to_cart('Monitor',2,'1')
+# เพิ่มสินค้า Keyboard จำนวน 3 ชิ้น ลงตะกร้าของไอดี 1 (MAX)
+bananaIT.Add_to_cart('Keyboard',3,'1')
+
+
+#test add cart
+print('ID :',maxkey.get_acc_id(),'| Name :',  maxkey.get_name() ,'| Cart :', maxkey.get_cart_shopping())
+print('ID :',jj.get_acc_id(),'| Name :',  jj.get_name() ,'| Cart :', jj.get_cart_shopping())
